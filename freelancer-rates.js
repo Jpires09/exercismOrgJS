cc92a86e85647111123a8f30ba03bf4a3e50fba1
Jean <jeancpires09@gmail.com>
2023-05-14 11:23:40 -0300

    freelancer-rate.js incomplete

---
+++ freelancer-rates.js
@@ -0,0 +1,36 @@
+// @ts-check
+
+/**
+ * The day rate, given a rate per hour
+ *
+ * @param {number} ratePerHour
+ * @returns {number} the rate per day
+ */
+/*export*/ function dayRate(ratePerHour, hourJorney = 8) {
+    return ratePerHour * hourJorney
+}
+  
+  /**
+   * Calculates the number of days in a budget, rounded down
+   *
+   * @param {number} budget: the total budget
+   * @param {number} ratePerHour: the rate per hour
+   * @returns {number} the number of days
+   */
+  /*export*/ function daysInBudget(budget, ratePerHour) {
+    return Math.ceil(budget / dayRate(ratePerHour))
+}
+  
+  /**
+   * Calculates the discounted rate for large projects, rounded up
+   *
+   * @param {number} ratePerHour
+   * @param {number} numDays: number of days the project spans
+   * @param {number} discount: for example 20% written as 0.2
+   * @returns {number} the rounded up discounted rate
+   */
+  /*export*/ function priceWithMonthlyDiscount(ratePerHour, numDays, discount) {
+    return Math.round((numDays * dayRate(ratePerHour) * discount) / 22)
+}
+  
+console.log(daysInBudget(20000,89))
